b46d31375bd3f4c0c4d31c37a09fe60941262052
Felipe Cruz <krivozznova@gmail.com>
2025-08-06 16:23:47 -0600

    valid anagram exercise done

---
+++ AlgorithmLibrary/Entities/ValidAnagramEntity.cs
@@ -0,0 +1,62 @@
+﻿namespace AlgorithmLibrary.Entities;
+
+public class ValidAnagramEntity
+{
+    public bool Execute(string s, string t)
+    {
+
+        s = s.ToLower();
+        t = t.ToLower();
+
+        s.Replace(" ", "");
+        t.Replace(" ", "");
+
+        int[] array = new int[26];
+
+    }
+
+    public bool ProposalExecution(string s, string t)
+    {
+        // Is not a valid anagram if is not using ALL the original letters exactly once.
+        if (s.Length != t.Length) return false;
+
+        // collect each repetition for the two strings once we have the same lenght
+        Dictionary<string, int> sDict = new Dictionary<string, int>();
+        Dictionary<string, int> tDict = new Dictionary<string, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (sDict.ContainsKey($"{s[i]}"))
+            {
+                sDict[$"{s[i]}"] = sDict[$"{s[i]}"] + 1;
+            }
+            else
+            {
+                sDict[$"{s[i]}"] = 1;
+            }
+
+
+
+            if (tDict.ContainsKey($"{t[i]}"))
+            {
+                tDict[$"{t[i]}"] = tDict[$"{t[i]}"] + 1;
+            }
+            else
+            {
+                tDict[$"{t[i]}"] = 1;
+            }
+
+        }
+
+        // comparing dictionaries values
+        foreach (string subS in sDict.Keys)
+        {
+            if (!tDict.ContainsKey(subS)) return false;
+
+            if (tDict[subS] != sDict[subS]) return false;
+
+        }
+
+        return true;
+    }
+}
---
+++ AlgorithmLibrary/Services/ManipulationHashing/ValidAnagramService.cs
@@ -0,0 +1,36 @@
+﻿using AlgorithmLibrary.Contracts;
+using AlgorithmLibrary.Entities;
+
+namespace AlgorithmLibrary.Services.ManipulationHashing;
+
+public class ValidAnagramService : IValidAnagramService
+{
+    public Task<Dictionary<string, bool>> demonstrate()
+    {
+        Dictionary<string, bool> results = new Dictionary<string, bool>();
+        ValidAnagramEntity entity = new ValidAnagramEntity();
+
+        // test 1
+        string s1 = "anagram";
+        string t1 = "nagaram";
+
+        bool res1 = entity.Execute(s1, t1);
+        results.Add($"s1 {s1}, t1: {t1}", res1);
+
+        //// test 2
+        string s2 = "state";
+        string t2 = "tea";
+
+        bool res2 = entity.Execute(s2, t2);
+        results.Add($"s2 {s2}, t2: {t2}", res2);
+
+        //// test 3
+        string s3 = "school master";
+        string t3 = "the classroom";
+
+        bool res3 = entity.Execute(s3, t3);
+        results.Add($"s3 {s3}, t3: {t3}", res3);
+
+        return Task.FromResult(results);
+    }
+}
